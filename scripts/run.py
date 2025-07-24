07ffe3796789bf21d66b75ddf22eef1cd7edcc46
Nguyen Minh Quang <quang.nm.robotics@gmail.com>
2025-07-24 17:09:57 +0700

    Minor changes

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from adaptive_coverage.utils.arg_parse import get_args
+from adaptive_coverage.utils.arg_parse import get_args, save_configs
 from adaptive_coverage.simulator.data_manager import LogManager, ResultManager
 from adaptive_coverage.swarms.hexagon_swarm import HexagonSwarm
 from adaptive_coverage.swarms.voronoi_swarm import VoronoiSwarm
@@ -13,123 +13,100 @@ from adaptive_coverage.simulator.renderer import Renderer
 def run():
     args = get_args("configs/default_args.yaml")
 
-    num_agents = args.num_agents
-    agent_size = args.agent_size
-    v_max = args.v_max
-    tolerance = args.tolerance
-    avoidance_range = args.avoidance_range
-    sensing_range = args.sensing_range
-
-    first_agent_pos = args.first_agent_pos
-
-    kg = args.goal_factor
-    ko = args.obstacle_factor
-    kc = args.collision_factor
-    beta_c = args.beta_c
-    scale = args.scale
-    rho = args.rho
-    pso_weights = np.array(args.pso_weights)
-    controller = args.controller
-    original_method = args.original_method
-
     path_planner = ArtificialPotentialField(
-        kg, ko, kc, beta_c, sensing_range, avoidance_range, agent_size
+        kg=args.goal_factor,
+        ko=args.obstacle_factor,
+        kc=args.collision_factor,
+        beta_c=args.beta_c,
+        sensing_range=args.sensing_range,
+        avoidance_range=args.sensing_range,
+        agent_size=args.agent_size,
     )
 
-    screen_size = args.screen_size
-
-    area_width = args.area_width
-    area_height = args.area_height
-    obstacles = np.array(args.obstacles)
-
-    timesteps = args.timesteps
-
-    res_dir = args.res_dir
-    log_dir = args.log_dir
-    env_dir = args.env
     log_manager = LogManager(
-        num_agents=num_agents,
-        log_dir=log_dir,
-        env_dir=env_dir,
-        controller=controller,
-        original_method=original_method,
+        num_agents=args.num_agents,
+        log_dir=args.log_dir,
+        env_dir=args.env,
+        controller=args.controller,
+        original_method=args.original_method,
     )
     result_manager = ResultManager(
-        num_agents=num_agents,
-        res_dir=res_dir,
-        env_dir=env_dir,
-        controller=controller,
-        original_method=original_method,
+        num_agents=args.num_agents,
+        res_dir=args.res_dir,
+        env_dir=args.env,
+        controller=args.controller,
+        original_method=args.original_method,
     )
 
-    if controller == "hexagon":
-        pso_num_particles = args.pso_num_particles
-        pso_num_iterations = args.pso_iterations
+    if args.controller == "hexagon":
         swarm = HexagonSwarm(
-            num_agents=num_agents,
-            original_method=original_method,
-            v_max=v_max,
-            avoidance_range=avoidance_range,
-            tolerance=tolerance,
-            agent_size=agent_size,
-            path_planner=path_planner,
-            sensing_range=sensing_range,
-            first_agent_pos=first_agent_pos,
-            rho=rho,
-            pso_weights=pso_weights,
-            pso_num_particles=pso_num_particles,
-            pso_num_iterations=pso_num_iterations,
+            num_agents=args.num_agents,
+            original_method=args.original_method,
+            v_max=args.v_max,
+            avoidance_range=args.avoidance_range,
+            tolerance=args.tolerance,
+            agent_size=args.agent_size,
+            path_planner=args.path_planner,
+            sensing_range=args.sensing_range,
+            first_agent_pos=args.first_agent_pos,
+            rho=args.rho,
+            pso_weights=args.pso_weights,
+            pso_num_particles=args.pso_num_particles,
+            pso_num_iterations=args.pso_num_iterations,
             result_manager=result_manager,
             log_manager=log_manager,
         )
     else:
         swarm = VoronoiSwarm(
-            num_agents=num_agents,
-            v_max=v_max,
-            avoidance_range=avoidance_range,
-            tolerance=tolerance,
-            agent_size=agent_size,
+            num_agents=args.num_agents,
+            v_max=args.v_max,
+            avoidance_range=args.avoidance_range,
+            tolerance=args.tolerance,
+            agent_size=args.agent_size,
             path_planner=path_planner,
-            sensing_range=sensing_range,
-            first_agent_pos=first_agent_pos,
+            sensing_range=args.sensing_range,
+            first_agent_pos=args.first_agent_pos,
             result_manager=result_manager,
             log_manager=log_manager,
         )
 
-    env = Environment(area_width, area_height, obstacles, offset=1)
+    env = Environment(args.area_width, args.area_height, args.obstacles, offset=1)
 
-    show_connections = args.show_connections
-    show_goal = args.show_goal
-    show_trajectories = args.show_trajectories
-    show_sensing_range = args.show_sensing_range
-
+    print(args.timestep)
+    print(args.total_time)
     sim = Simulator(
-        screen_size=screen_size,
+        screen_size=args.screen_size,
         swarm=swarm,
         env=env,
         result_manager=result_manager,
         log_manager=log_manager,
-        scale=scale,
-        timesteps=timesteps,
+        scale=args.scale,
+        total_time=args.total_time,
+        timestep=args.timestep,
     )
 
-    if not os.path.exists(result_manager.swarm_data_filepath):
-        sim.execute()
+    sim.execute()
+    render(args, log_manager, result_manager)
+    save_configs(args, os.path.join(result_manager.res_dir, "configs.yaml"))
 
+
+def render(args, log_manager, result_manager):
+    env = Environment(args.area_width, args.area_height, args.obstacles, offset=1)
     renderer = Renderer(
-        screen_size=screen_size,
+        screen_size=args.screen_size,
         trajectories_filepath=result_manager.swarm_data_filepath,
-        controller=controller,
-        agent_size=agent_size,
-        sensing_range=sensing_range,
-        swarm=swarm,
+        controller=args.controller,
+        agent_size=args.agent_size,
+        sensing_range=args.sensing_range,
+        result_manager=result_manager,
+        log_manager=log_manager,
         env=env,
-        scale=scale,
+        scale=args.scale,
         linewidth=args.linewidth,
-        show_connections=show_connections,
-        show_goal=show_goal,
-        show_sensing_range=show_sensing_range,
-        show_trajectories=show_trajectories,
+        show_connections=args.show_connections,
+        show_goal=args.show_goal,
+        show_sensing_range=args.show_sensing_range,
+        show_trajectories=args.show_trajectories,
     )
     renderer.run()
 
