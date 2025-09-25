c3d5ba8ac98bca489289dc3df3d4189a173bd9d5
Quang <quangnm0564@gmail.com>
2025-09-25 10:04:47 +0700

    Minor changes

--- scripts/run.bat
+++ scripts/run.bat
@@ -8,12 +8,26 @@
 
 :: Voronoi method
 
-start cmd /k python scripts/run.py --env env0 --show_connections true --show_sensing_range True --controller voronoi
+:: start cmd /k python scripts/run.py --env env0 --show_connections true --show_sensing_range True --controller voronoi
 
-start cmd /k python scripts/run.py --env env1 --show_connections true --show_sensing_range True --controller voronoi
+:: start cmd /k python scripts/run.py --env env1 --show_connections true --show_sensing_range True --controller voronoi
 
-start cmd /k python scripts/run.py --env env2 --show_connections true --show_sensing_range True --controller voronoi
+:: start cmd /k python scripts/run.py --env env2 --show_connections true --show_sensing_range True --controller voronoi
 
-start cmd /k python scripts/run.py --env env3 --show_connections true --show_sensing_range True --controller voronoi
+:: start cmd /k python scripts/run.py --env env3 --show_connections true --show_sensing_range True --controller voronoi
 
-start cmd /k python scripts/run.py --env env4 --show_connections true --show_sensing_range true --controller voronoi
+:: start cmd /k python scripts/run.py --env env4 --show_connections true --show_sensing_range true --controller voronoi
+
+::start cmd /k python scripts/run.py --env env0 --show_sensing_range True --controller voronoi
+
+::start cmd /k python scripts/run.py --env env1 --show_sensing_range True --controller voronoi
+
+::start cmd /k python scripts/run.py --env env2 --show_sensing_range True --controller voronoi
+
+::start cmd /k python scripts/run.py --env env3 --show_sensing_range True --controller voronoi
+
+start cmd /k python scripts/run.py --env env3 --show_sensing_range true --controller voronoi
+start cmd /k python scripts/run.py --env env3 --show_sensing_range true --controller hexagon --original_method false
+
+start cmd /k python scripts/run.py --env env4 --show_sensing_range true --controller voronoi
+start cmd /k python scripts/run.py --env env4 --show_sensing_range true --controller hexagon --original_method false
