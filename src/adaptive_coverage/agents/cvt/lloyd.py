e88a27e1dfdbade35272f57dfd51ab24364d6f81
Nguyen Minh Quang <quangnm0564@gmail.com>
2025-11-17 15:52:18 +0700

    Minor changes

--- src/adaptive_coverage/agents/cvt/lloyd.py
+++ src/adaptive_coverage/agents/cvt/lloyd.py
@@ -19,7 +19,7 @@ def density_func(q):
     return 1
 
 
-def centroid_region(agent, vertices, env, resolution=5):
+def centroid_region(agent, vertices, env, resolution=20):
     """
     Compute the centroid of the polygon using vectorized Trapezoidal rule on a grid.
     """
