0dbecd1d4cd563ccb818a271bfb39071f00784fa
Nguyen Minh Quang <quangnm0564@gmail.com>
2025-11-18 08:19:28 +0700

    - Add batch script to run hexagon-pso controller on all environment - Remove `communication_range` from config file

---
+++ scripts/run_hexagon_pso.bat
@@ -0,0 +1,8 @@
+@echo off
+
+start cmd /k python scripts/run.py --env env1 --show_sensing_range True --controller hexagon --original_method False
+start cmd /k python scripts/run.py --env env2 --show_sensing_range True --controller hexagon --original_method False
+start cmd /k python scripts/run.py --env env3 --show_sensing_range True --controller hexagon --original_method False
+start cmd /k python scripts/run.py --env env4 --show_sensing_range True --controller hexagon --original_method False
+start cmd /k python scripts/run.py --env env5 --show_sensing_range True --controller hexagon --original_method False
+start cmd /k python scripts/run.py --env env6 --show_sensing_range True --controller hexagon --original_method False
